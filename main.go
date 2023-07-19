8ce527163fb55671e675ddf4478e3eca08de7169
pete <p.milenov.bg@gmail.com>
2023-07-19 17:09:16 +0300

    some alteration

--- main.go
+++ main.go
@@ -1,116 +1,295 @@
+// package main
+
+// import (
+// 	"io/ioutil"
+// 	"log"
+// 	"net/http"
+// 	"os"
+// 	"path/filepath"
+// 	"strings"
+// 	"sync"
+// )
+
+// const (
+// 	maxBufferSize = 1 * 1024 * 16 // 16KB
+// 	outputDir     = "data"
+// )
+
+// type JSONData struct {
+// 	Data string `json:"data"`
+// }
+
+// type JSONParseError struct {
+// 	Msg string
+// }
+
+// func (e *JSONParseError) Error() string {
+// 	return e.Msg
+// }
+
+// func isBlackholePathValid(path string) bool {
+// 	// Remove leading and trailing slashes
+// 	path = strings.Trim(path, "/")
+// 	// Check if it has a valid name
+// 	if !isValidFilename(path) {
+// 		return false
+// 	}
+// 	// Check if the blackhole directory exists
+// 	blackholeDir := filepath.Join(outputDir, "blackhole")
+// 	info, err := os.Stat(blackholeDir)
+// 	if err != nil || !info.IsDir() {
+// 		return false
+// 	}
+// 	return true
+// }
+// func isValidFilename(filename string) bool {
+// 	// Check for special characters
+// 	if strings.ContainsAny(filename, `/\:*?"<>|`) {
+// 		return false
+// 	}
+// 	return true
+// }
+
+// func bufferJSONs(jsonChan <-chan []byte, bufferChan chan<- []byte, wg *sync.WaitGroup) {
+// 	buffer := []byte{}
+// 	bufferSize := 0
+
+// 	for json := range jsonChan {
+// 		buffer = append(buffer, json...)
+// 		bufferSize += len(json)
+
+// 		if bufferSize >= maxBufferSize {
+// 			bufferChan <- buffer
+// 			buffer = []byte{}
+// 			bufferSize = 0
+// 		}
+// 	}
+
+// 	if len(buffer) > 0 {
+// 		bufferChan <- buffer
+// 	}
+
+// 	close(bufferChan)
+// 	wg.Done()
+// }
+
+// func storeBuffer(bufferChan <-chan []byte, wg *sync.WaitGroup) {
+// 	file, err := os.OpenFile(getOutputFilename("/default"), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+// 	if err != nil {
+// 		log.Fatalf("Error opening output file: %v", err)
+// 	}
+// 	defer file.Close()
+
+// 	for buffer := range bufferChan {
+// 		_, err := file.Write(buffer)
+// 		if err != nil {
+// 			log.Printf("Error writing to output file: %v", err)
+// 		}
+// 	}
+
+// 	wg.Done()
+// }
+
+// func getOutputFilename(path string) string {
+// 	return outputDir + path + ".json"
+// }
+
+// func main() {
+// 	jsonChan := make(chan []byte)
+// 	bufferChan := make(chan []byte)
+
+// 	var wg sync.WaitGroup
+// 	wg.Add(2)
+
+// 	go bufferJSONs(jsonChan, bufferChan, &wg)
+// 	go storeBuffer(bufferChan, &wg)
+
+// 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+// 		if r.Method == http.MethodPost {
+// 			body, err := ioutil.ReadAll(r.Body)
+// 			if err != nil {
+// 				http.Error(w, "Error reading request body", http.StatusInternalServerError)
+// 				return
+// 			}
+// 			defer r.Body.Close()
+
+// 			path := r.URL.Path
+// 			if path == "/" {
+// 				path = "/default"
+// 			}
+
+// 			if strings.HasPrefix(path, "/blackhole") {
+// 				if !isBlackholePathValid(path) {
+// 					log.Printf("Invalid blackhole path: %s", path)
+// 					http.Error(w, "Invalid blackhole path", http.StatusBadRequest)
+// 					return
+// 				}
+
+// 				outputFile := getOutputFilename(path)
+
+// 				jsonChan <- body
+
+// 				w.Write([]byte("Request received and processed"))
+// 				log.Printf("JSON data received and stored in file: %s", outputFile)
+// 			} else {
+// 				w.Write([]byte("Invalid request method"))
+// 			}
+// 		}
+// 	})
+
+// 	log.Fatal(http.ListenAndServe(":8080", nil))
+
+// 	close(jsonChan)
+// 	wg.Wait()
+// }
+
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
+	"strings"
 	"sync"
 )
 
-var buffer []interface{}
-var mutex sync.Mutex
+const (
+	maxBufferSize = 1 * 1024 * 16 // 16KB
+	outputDir     = "data"
+)
+
+type JSONData struct {
+	Data string `json:"data"`
+}
+
+type JSONParseError struct {
+	Msg string
+}
+
+func (e *JSONParseError) Error() string {
+	return e.Msg
+}
 
 func main() {
-	http.HandleFunc("/", handleRequest)
+	// Create the output directory if it doesn't exist
+	err := os.MkdirAll(outputDir, 0755)
+	if err != nil {
+		log.Fatal("Error creating output directory:", err)
+	}
+
+	jsonChan := make(chan []byte)
+	bufferChan := make(chan []byte)
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go bufferJSONs(jsonChan, bufferChan, &wg)
+	go storeBuffer(bufferChan, &wg)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost {
+			body, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				http.Error(w, "Error reading request body", http.StatusInternalServerError)
+				return
+			}
+			defer r.Body.Close()
+
+			path := r.URL.Path
+			if path == "/" {
+				path = "/default"
+			}
+
+			if strings.HasPrefix(path, "/blackhole") {
+				if !isBlackholePathValid(path) {
+					log.Printf("Invalid blackhole path: %s", path)
+					http.Error(w, "Invalid blackhole path", http.StatusBadRequest)
+					return
+				}
+
+				outputFile := getOutputFilename(path)
+
+				jsonChan <- body
+
+				w.Write([]byte("Request received and processed"))
+				log.Printf("JSON data received and stored in file: %s", outputFile)
+			} else {
+				jsonChan <- body
+
+				w.Write([]byte("Request received"))
+			}
+		} else {
+			w.Write([]byte("Invalid request method"))
+		}
+	})
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
+
+	close(jsonChan)
+	wg.Wait()
 }
 
-func handleRequest(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case http.MethodGet:
-		handleGetRequest(w, r)
-	case http.MethodPost:
-		handlePostRequest(w, r)
-	case http.MethodPut:
-		handlePutRequest(w, r)
-	case http.MethodDelete:
-		handleDeleteRequest(w, r)
-	default:
-		http.Error(w, "Method not supported", http.StatusMethodNotAllowed)
-	}
-}
-
-func handleGetRequest(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "GET request received")
-}
-
-func handlePostRequest(w http.ResponseWriter, r *http.Request) {
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "Error reading request body", http.StatusInternalServerError)
-		return
-	}
-	defer r.Body.Close()
-
-	var data interface{}
-	err = json.Unmarshal(body, &data)
-	if err != nil {
-		http.Error(w, "Invalid JSON body", http.StatusBadRequest)
-		return
-	}
-
-	mutex.Lock()
-	buffer = append(buffer, data)
-	bufferSize := calculateBufferSize()
-	mutex.Unlock()
-
-	if bufferSize >= 0.5*1024*16 {
-		go writeBufferedDataToFile()
-	}
-
-	fmt.Fprint(w, "POST request received")
-}
-
-func handlePutRequest(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "PUT request received")
-}
-
-func handleDeleteRequest(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "DELETE request received")
-}
-
-func calculateBufferSize() int {
+func bufferJSONs(jsonChan <-chan []byte, bufferChan chan<- []byte, wg *sync.WaitGroup) {
+	buffer := []byte{}
 	bufferSize := 0
-	for _, data := range buffer {
-		jsonData, err := json.Marshal(data)
-		if err != nil {
-			log.Println("Error marshaling JSON data:", err)
-			continue
+
+	for json := range jsonChan {
+		buffer = append(buffer, json...)
+		bufferSize += len(json)
+
+		if bufferSize >= maxBufferSize {
+			bufferChan <- buffer
+			buffer = []byte{}
+			bufferSize = 0
 		}
-		bufferSize += len(jsonData)
 	}
-	return bufferSize
+
+	if len(buffer) > 0 {
+		bufferChan <- buffer
+	}
+
+	close(bufferChan)
+	wg.Done()
 }
 
-func writeBufferedDataToFile() {
-	mutex.Lock()
-	defer mutex.Unlock()
-	// --- test of the writing to the file -----
-	file, err := os.OpenFile("received-jsonl", os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
-	if err != nil {
-		log.Println("Error opening file:", err)
-		return
-	}
-	defer file.Close()
+func storeBuffer(bufferChan <-chan []byte, wg *sync.WaitGroup) {
+	for buffer := range bufferChan {
+		outputFile := filepath.Join(outputDir, "received-jsons")
 
-	for _, data := range buffer {
-		jsonData, err := json.Marshal(data)
+		err := ioutil.WriteFile(outputFile, buffer, 0644)
 		if err != nil {
-			log.Println("Error marshaling JSON data:", err)
-			continue
-		}
-
-		_, err = file.Write(jsonData)
-		if err != nil {
-			log.Println("Error writing to file:", err)
+			log.Printf("Error writing to output file: %v", err)
 		}
 	}
-	// --- test of the writing to the file -----
-	buffer = nil
-	fmt.Println("Buffered data written to file")
+
+	wg.Done()
 }
 
-func dumpt()
+func isBlackholePathValid(path string) bool {
+	// Remove leading and trailing slashes
+	path = strings.Trim(path, "/")
+
+	// Check if it has a valid name
+	if !isValidFilename(path) {
+		return false
+	}
+	return true
+}
+
+func isValidFilename(filename string) bool {
+	// Check for special characters
+	if strings.ContainsAny(filename, `/\:*?"<>|`) {
+		return false
+	}
+
+	return true
+}
+
+func getOutputFilename(path string) string {
+	// Remove leading and trailing slashes
+	path = strings.Trim(path, "/")
+
+	return filepath.Join(outputDir, "blackhole", path)
+}
